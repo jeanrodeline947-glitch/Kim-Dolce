a633dc0caa9fb02f387895f56445759a7e1b5176
jeanrodeline947-glitch <jeanrodeline947@gmail.com>
2026-08-01 09:28:15 -0400

    Add configuration file for bot settings

---
+++ bot/config.js
@@ -0,0 +1,25 @@
+"use strict";
+
+module.exports = {
+  botName: "KIM-BOT",
+  ownerName: "KIM DOLCE",
+  prefix: ".",
+
+  version: "2.0.0",
+
+  // Website
+  website: {
+    enabled: true,
+    port: 3000
+  },
+
+  // WhatsApp session
+  sessionFolder: "session",
+
+  // Bot settings
+  settings: {
+    autoRead: false,
+    autoTyping: false,
+    antiSpam: true
+  }
+};
